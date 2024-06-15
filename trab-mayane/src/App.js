a2f9969745bca5e5d8f4678039be3f7d159c2498
Mayane Lopes <mayaneclopes@gmail.com>
2024-06-15 18:53:47 -0300

    criação pag de login simples

---
+++ trab-mayane/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  }
+])
+
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
